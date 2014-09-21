d51c8cda7cf81f91afc0d38a1306cdb65e1e0890
Jamie Storey <j.storey@northumbria.ac.uk>
2014-09-21 16:39:25 +0100

    Added gui and main method.

---
+++ UNI/src/me/jamiestorey/java/gui/swing/Main.java
@@ -0,0 +1,9 @@
+
+package me.jamiestorey.java.gui.swing;
+
+public class Main {
+
+	public static void main(String[] args) {
+		new Window();
+	}
+}
---
+++ UNI/src/me/jamiestorey/java/gui/swing/Window.java
@@ -0,0 +1,19 @@
+
+package me.jamiestorey.java.gui.swing;
+
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class Window {
+
+	public Window() {
+		JFrame frame = new JFrame("A Window");
+		frame.setPreferredSize(new Dimension(800, 600));
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+	}
+
+}
