6b4cabaf2ccd78b1f1c9ceeb45ffe1b9eab8a50f
Jamie Storey <j.storey@northumbria.ac.uk>
2014-10-26 19:19:44 +0000

    Added NimGame to Week04 lab

---
+++ UNI/src/com/jamiestorey/uni/java/labs/week04/nimgame/Pile.java
@@ -0,0 +1,53 @@
+
+package com.jamiestorey.uni.java.labs.week04.nimgame;
+
+/**
+ * Models a pile of stickes.
+ * 
+ * @author Ian Bradley
+ * @version V1
+ */
+public class Pile
+{
+
+	// instance variables - replace the example below with your own
+	private int sticksLeft;
+
+	/**
+	 * Constructor for objects of class Pile
+	 */
+	public Pile(int initialSticks)
+	{
+		sticksLeft = initialSticks;
+	}
+
+	/**
+	 * gets the number of sticks left in the pile.
+	 * 
+	 * @return the number of sticks left
+	 */
+	public int getStcksLeft()
+	{
+		return sticksLeft;
+	}
+
+	/**
+	 * Reduces the number of sticks by the specified amount.
+	 *
+	 * @param number
+	 *            the number to remove, must be greater than 0 and less than
+	 *            number Left
+	 * @return true if succesful otherwise false
+	 */
+	public boolean removeSticks(int number)
+	{
+		if ((number > 0) && (number <= sticksLeft))
+		{
+
+			sticksLeft = sticksLeft - number;
+			return true;
+		}
+		return false;
+	}
+
+}
---
+++ UNI/src/com/jamiestorey/uni/java/labs/week04/nimgame/Player.java
@@ -0,0 +1,71 @@
+
+package com.jamiestorey.uni.java.labs.week04.nimgame;
+
+/**
+ * A Player fro a simple nim game.
+ * 
+ * @author Ian Bradley
+ * @version 1.0
+ */
+public class Player
+{
+
+	private String name;
+	private int numberTaken;
+
+	/**
+	 * Constructor for objects of class Player
+	 * 
+	 * @param name
+	 *            the name of the palyer
+	 */
+	public Player(String name)
+	{
+		this.name = name;
+		numberTaken = 0;
+	}
+
+	/**
+	 * gets the name of the player
+	 * 
+	 * @return the name of the player
+	 */
+	public String getName()
+	{
+		return name;
+	}
+
+	/**
+	 * Get the number of sticks taken on last turn
+	 *
+	 * @return the number of sticks taken on last turn
+	 */
+	public int getNumberTaken()
+	{
+		return numberTaken;
+	}
+
+	/**
+	 * Remove the number of sticks specified from the given pile.
+	 *
+	 * @param number
+	 *            number of sticks to to be removed, >0
+	 * @param pile
+	 *            the pile to be reduced
+	 * @return true if successful otherwise false
+	 */
+	public boolean takeTurn(int number, Pile pile)
+	{
+		boolean success = pile.removeSticks(number);
+		if (success)
+		{
+			numberTaken = number;
+		}
+		else
+		{
+			numberTaken = 0;
+		}
+		return success;
+	}
+
+}
