3d38465877280aae0cf6aba06c61c5ab746fed65
Jamie Storey <j.storey@northumbria.ac.uk>
2014-11-02 21:49:24 +0000

    Added week 07 exercises

---
+++ UNI/src/com/jamiestorey/uni/java/labs/week07/exercises/Library.java
@@ -0,0 +1,54 @@
+
+package com.jamiestorey.uni.java.labs.week07.exercises;
+
+import java.util.ArrayList;
+
+/**
+ * Write a description of class Library here.
+ * 
+ * @author Jamie Storey
+ * @version 2/11/14
+ */
+public class Library
+{
+
+	private ArrayList<SimpleBook> books;
+
+	/**
+	 * Constructor for objects of class Library
+	 */
+	public Library()
+	{
+		books = new ArrayList<SimpleBook>();
+	}
+
+	public void addBook(SimpleBook book)
+	{
+		books.add(book);
+	}
+
+	public int getNumberOfBooks()
+	{
+		return books.size();
+	}
+
+	public int findFirstIndexOf(SimpleBook book)
+	{
+		return books.indexOf(book);
+	}
+
+	public int findLastIndexOf(SimpleBook book)
+	{
+		return books.lastIndexOf(book);
+	}
+
+	public int search3(SimpleBook targetBook)
+	{
+		for (SimpleBook book : books)
+		{
+			if (book.equals(targetBook)) { return books.indexOf(book); }
+		}
+		return -1;
+	}
+
+}
---
+++ UNI/src/com/jamiestorey/uni/java/labs/week07/exercises/SimpleBook.java
@@ -0,0 +1,80 @@
+
+package com.jamiestorey.uni.java.labs.week07.exercises;
+
+/**
+ * A class that maintains information on a book. This might form part of a
+ * larger application such as a library system, for instance.
+ * 
+ * Simplified for Week 6 lectoral exercise 2
+ * 
+ * Contains the exercise solution
+ * 
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class SimpleBook
+{
+
+	// The fields
+	private String author;
+	private String title;
+
+	/**
+	 * Set the author and title fields when this object is constructed.
+	 * 
+	 * @param bookAuthor
+	 *            the name of the author of the book
+	 * @param bookTitle
+	 *            the name of the book.
+	 */
+	public SimpleBook(String bookAuthor, String bookTitle)
+	{
+		author = bookAuthor;
+		title = bookTitle;
+	}
+
+	public boolean equals(Object obj)
+	{
+		if (obj == null) { return false; }
+
+		if (this == obj) { return true; }
+
+		SimpleBook other = (SimpleBook) obj;
+
+		if ((other.getAuthor().equals(author))
+				&& (other.getTitle().equals(title)))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	/**
+	 * Returns the author of this book.
+	 * 
+	 * @return the author's name
+	 */
+	public String getAuthor()
+	{
+		return author;
+	}
+
+	/**
+	 * Returns the title of this book.
+	 * 
+	 * @return the book's title
+	 */
+	public String getTitle()
+	{
+		return title;
+	}
+
+	public String toString()
+	{
+		return author + ": " + title;
+	}
+
+}
